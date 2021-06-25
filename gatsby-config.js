7e4698d93e83c44824414a72964a196c5ce07a8d
Thijmen Maus <thijmen.maus@mundaro.nl>
2021-06-25 10:11:00 +0200

    Basic structure

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: "risingsun-website",
+  },
+  plugins: [],
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+function IndexPage() {
+    return (
+        <main>
+            <title>Index page</title>
+            <h1>Test</h1>
+            <p>cool</p>
+        </main>
+    );
+}
+
+export default IndexPage;
